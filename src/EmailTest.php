3bd1218e28b2fbc13291b59cf5dfc8c3e6d60512
hyper-vlad <hypervlados@gmail.com>
2020-08-07 15:49:09 +0300

    PHP Testing ☢

---
+++ src/EmailTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/** @test */
+final class EmailTest extends TestCase
+{
+    public function testEmailAddress(): void
+    {
+        $this->assertEquals(1,1);
+    }
+
+}
