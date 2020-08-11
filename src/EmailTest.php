43a94affaf5cd13a010d44f00e798eff66a08a8e
hyper-vlad <hypervlados@gmail.com>
2020-08-11 13:53:37 +0300

    PHP Email Test Assert

--- src/EmailTest.php
+++ src/EmailTest.php
@@ -7,7 +7,20 @@ final class EmailTest extends TestCase
 {
     public function testEmailAddress(): void
     {
+        $arrayEmail = [
+            'io' => 'false',
+            'www' => 'true',
+            '' => 'nan'
+        ];
+
+        $this->assertArrayHasKey('', $arrayEmail);
+
+        $this->assertCount(3, $arrayEmail);
+
+        $this->assertEquals('true', $arrayEmail['www']);
+
         $this->assertEquals(1,1);
+
         $this->assertEquals(2,2);
     }
 
